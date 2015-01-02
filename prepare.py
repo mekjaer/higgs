439ce00a54dc24b953435c0b71db2f45a2ba70cb
mekjaer <Morten@Mekbook-Air.local>
2015-01-02 23:56:24 +0100

    print entry[0]

--- prepare.py
+++ prepare.py
@@ -1,12 +1,13 @@
 
 raw_instances = []
 
-with open('HIGGS.csv', 'r') as f:
+with open('../HIGGS.csv', 'r') as f:
 	# use iterator to skip the first label row
 	lineIter = iter(f)
 	max = 0
 	for line in lineIter:
 		entry = line.strip().split(',')
+		print entry[0]
 		entry[0] = int(float(entry[0]))
 		raw_instances.append(entry)
 		max = max + 1
